20c4ce313bcb26a3a7604091525a8ce6520fa8a4
victor <victorsantot@hotmail.com>
2019-04-07 17:15:44 +0200

    Cambios

--- src/main/java/Vista/vistaReserva.java
+++ src/main/java/Vista/vistaReserva.java
@@ -12,12 +12,18 @@ public class vistaReserva extends JPanel
 	public  JButton Continuar_Reserva = new JButton("Continuar");
 	private JLabel lblHotelSeleccionado;
 	private JLabel lblPrecio;
-	private JLabel lblNumeroHabitaciones;
+	private JLabel lblNumeroPersonas;
 	private JLabel lblUbicacion;
+	private JLabel lblFechaDeEntrada;
+	private JLabel lblFechaDeSalida;
+	private JLabel lblNumeroHabitaciones;
 	public JTextField textField_ubicacion;
 	public JTextField textField_numpersonas;
 	public JTextField textField_precio;
 	public JTextField textField_hotelseleccionado;
+	public JTextField textField_fechaDeEntrada;
+	public JTextField textField_fechaDeSalida;
+	public JTextField textField_NumeroHabitaciones;
 	
 	public vistaReserva()
 	{
@@ -30,16 +36,16 @@ public class vistaReserva extends JPanel
 		add(Cancelar_Reserva);
 		
 		lblHotelSeleccionado = new JLabel("Hotel seleccionado:");
-		lblHotelSeleccionado.setBounds(238, 144, 124, 16);
+		lblHotelSeleccionado.setBounds(238, 98, 124, 16);
 		add(lblHotelSeleccionado);
 		
 		lblPrecio = new JLabel("Precio:");
-		lblPrecio.setBounds(320, 192, 42, 16);
+		lblPrecio.setBounds(320, 146, 42, 16);
 		add(lblPrecio);
 		
-		lblNumeroHabitaciones = new JLabel("Numero personas:");
-		lblNumeroHabitaciones.setBounds(247, 240, 115, 16);
-		add(lblNumeroHabitaciones);
+		lblNumeroPersonas = new JLabel("Numero personas:");
+		lblNumeroPersonas.setBounds(247, 194, 115, 16);
+		add(lblNumeroPersonas);
 		
 		lblUbicacion = new JLabel("Ubicacion:");
 		lblUbicacion.setBounds(296, 298, 66, 16);
@@ -56,21 +62,54 @@ public class vistaReserva extends JPanel
 		textField_numpersonas.setEditable(false);
 		textField_numpersonas.setHorizontalAlignment(SwingConstants.RIGHT);
 		textField_numpersonas.setColumns(10);
-		textField_numpersonas.setBounds(495, 235, 214, 26);
+		textField_numpersonas.setBounds(495, 189, 214, 26);
 		add(textField_numpersonas);
 		
 		textField_precio = new JTextField();
 		textField_precio.setEditable(false);
 		textField_precio.setHorizontalAlignment(SwingConstants.RIGHT);
 		textField_precio.setColumns(10);
-		textField_precio.setBounds(495, 187, 214, 26);
+		textField_precio.setBounds(495, 141, 214, 26);
 		add(textField_precio);
 		
 		textField_hotelseleccionado = new JTextField();
 		textField_hotelseleccionado.setEditable(false);
 		textField_hotelseleccionado.setHorizontalAlignment(SwingConstants.RIGHT);
 		textField_hotelseleccionado.setColumns(10);
-		textField_hotelseleccionado.setBounds(495, 139, 214, 26);
+		textField_hotelseleccionado.setBounds(495, 93, 214, 26);
 		add(textField_hotelseleccionado);
+		
+		lblFechaDeEntrada = new JLabel("Fecha de entrada:");
+		lblFechaDeEntrada.setBounds(252, 350, 110, 16);
+		add(lblFechaDeEntrada);
+		
+		lblFechaDeSalida = new JLabel("Fecha de salida:");
+		lblFechaDeSalida.setBounds(262, 404, 100, 16);
+		add(lblFechaDeSalida);
+		
+		textField_fechaDeEntrada = new JTextField();
+		textField_fechaDeEntrada.setHorizontalAlignment(SwingConstants.RIGHT);
+		textField_fechaDeEntrada.setEditable(false);
+		textField_fechaDeEntrada.setColumns(10);
+		textField_fechaDeEntrada.setBounds(495, 345, 214, 26);
+		add(textField_fechaDeEntrada);
+		
+		textField_fechaDeSalida = new JTextField();
+		textField_fechaDeSalida.setHorizontalAlignment(SwingConstants.RIGHT);
+		textField_fechaDeSalida.setEditable(false);
+		textField_fechaDeSalida.setColumns(10);
+		textField_fechaDeSalida.setBounds(495, 399, 214, 26);
+		add(textField_fechaDeSalida);
+		
+		lblNumeroHabitaciones = new JLabel("Numero habitaciones:");
+		lblNumeroHabitaciones.setBounds(227, 245, 138, 16);
+		add(lblNumeroHabitaciones);
+		
+		textField_NumeroHabitaciones = new JTextField();
+		textField_NumeroHabitaciones.setHorizontalAlignment(SwingConstants.RIGHT);
+		textField_NumeroHabitaciones.setEditable(false);
+		textField_NumeroHabitaciones.setColumns(10);
+		textField_NumeroHabitaciones.setBounds(495, 240, 214, 26);
+		add(textField_NumeroHabitaciones);
 	}
 }
