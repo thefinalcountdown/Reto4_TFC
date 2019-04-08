23e05ea02125bf71733861cdcf505675709577ca
merylabu <maria.laburuha@elorrieta-errekamari.com>
2019-04-08 21:18:44 +0200

    Update controladorRaiz.java

--- src/main/java/Controlador/controladorRaiz.java
+++ src/main/java/Controlador/controladorRaiz.java
@@ -6,8 +6,6 @@ import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.util.ArrayList;
 import java.util.Date;
-import org.apache.commons.lang.time.DateUtils;
-
 import com.toedter.calendar.JDateChooser;
 
 import Modelo.modelo;
