d4dc57365dbbb0b711c518f98764d52d3c86efa0
Vic <victorsantot@hotmail.com>
2019-04-11 19:54:40 +0200

    orden

--- src/main/java/Controlador/controladorLogin.java
+++ src/main/java/Controlador/controladorLogin.java
@@ -19,59 +19,82 @@ public class controladorLogin {
 		this.ventana = ventana;
 		this.modelo = modelo;
 
-		ventana.login.btnLogin.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
+		ventana.login.btnLogin.addActionListener(new ActionListener() 
+		{
+			public void actionPerformed(ActionEvent e) 
+			{
 				if (funcionesLogin.comprobarDni(GestorBD.consulta(funcionesLogin
-						.sentenciaCombrobarDni(ventana.login.formattedTextFieldLoginDNI.getText()))) == false) {
+						.sentenciaCombrobarDni(ventana.login.formattedTextFieldLoginDNI.getText()))) == false) 
+				{
 					JOptionPane.showMessageDialog(null, "El usuario no existe...");
-				} else if (funcionesLogin.comprobarLogin(GestorBD
+				} 
+				else if (funcionesLogin.comprobarLogin(GestorBD
 						.consulta(funcionesLogin.sentenciaLogin(ventana.login.formattedTextFieldLoginDNI.getText(),
-								String.valueOf(ventana.login.passwordFieldLoginClave.getPassword())))) == true) {
+								String.valueOf(ventana.login.passwordFieldLoginClave.getPassword())))) == true) 
+				{
 					JOptionPane.showMessageDialog(null, "Usuario logueado con exito...");
 					ventana.cambio_panel(ventana.login, ventana.raiz);
-				} else {
+				} 
+				else 
+				{
 					JOptionPane.showMessageDialog(null, "Clave incorrecta...");
 				}
 			}
 		});
 
-		ventana.login.btnActualizar.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
+		ventana.login.btnActualizar.addActionListener(new ActionListener() 
+		{
+			public void actionPerformed(ActionEvent arg0) 
+			{
 				if (funcionesLogin.comprobarDni(GestorBD.consulta(funcionesLogin
-						.sentenciaCombrobarDni(ventana.login.formattedTextFieldActualizarDNI.getText()))) == false) {
+						.sentenciaCombrobarDni(ventana.login.formattedTextFieldActualizarDNI.getText()))) == false) 
+				{
 					JOptionPane.showMessageDialog(null, "El usuario no existe...");
-				} else if (funcionesLogin.comprobarLogin(GestorBD
+				} 
+				else if (funcionesLogin.comprobarLogin(GestorBD
 						.consulta(funcionesLogin.sentenciaLogin(ventana.login.formattedTextFieldActualizarDNI.getText(),
-								String.valueOf(ventana.login.passwordFieldActualizarClave.getPassword())))) == false) {
+								String.valueOf(ventana.login.passwordFieldActualizarClave.getPassword())))) == false) 
+				{
 					JOptionPane.showMessageDialog(null, "Clave incorrecta...");
-				} else if (GestorBD.insertarDatos(funcionesLogin.sentenciaActualizarUsuario(
+				} 
+				else if (GestorBD.insertarDatos(funcionesLogin.sentenciaActualizarUsuario(
 						ventana.login.formattedTextFieldActualizarDNI.getText(),
 						String.valueOf(ventana.login.passwordFieldActualizarClave.getPassword()),
-						String.valueOf(ventana.login.passwordFieldNuevaClave.getPassword()))) == true) {
+						String.valueOf(ventana.login.passwordFieldNuevaClave.getPassword()))) == true) 
+				{
 					JOptionPane.showMessageDialog(null, "Clave actualizada con exito...");
 				}
 			}
 		});
 
-		ventana.login.btnBorrarUsuario.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
+		ventana.login.btnBorrarUsuario.addActionListener(new ActionListener() 
+		{
+			public void actionPerformed(ActionEvent arg0) 
+			{
 				if (funcionesLogin.comprobarDni(GestorBD.consulta(funcionesLogin
-						.sentenciaCombrobarDni(ventana.login.formattedTextFieldLoginDNI.getText()))) == false) {
+						.sentenciaCombrobarDni(ventana.login.formattedTextFieldLoginDNI.getText()))) == false) 
+				{
 					JOptionPane.showMessageDialog(null, "El usuario no existe...");
-				} else if (funcionesLogin.comprobarLogin(GestorBD
+				}
+				else if (funcionesLogin.comprobarLogin(GestorBD
 						.consulta(funcionesLogin.sentenciaLogin(ventana.login.formattedTextFieldLoginDNI.getText(),
-								String.valueOf(ventana.login.passwordFieldLoginClave.getPassword())))) == false) {
+								String.valueOf(ventana.login.passwordFieldLoginClave.getPassword())))) == false) 
+				{
 					JOptionPane.showMessageDialog(null, "Clave incorrecta...");
-				} else if (GestorBD.insertarDatos(
+				} 
+				else if (GestorBD.insertarDatos(
 						funcionesLogin.sentenciaBorrarUsuario(ventana.login.formattedTextFieldLoginDNI.getText(),
-								String.valueOf(ventana.login.passwordFieldLoginClave.getPassword()))) == true) {
+								String.valueOf(ventana.login.passwordFieldLoginClave.getPassword()))) == true) 
+				{
 					JOptionPane.showMessageDialog(null, "Usuario borrado correctamente");
 				}
 			}
 		});
 
-		ventana.login.btnCancelar.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
+		ventana.login.btnCancelar.addActionListener(new ActionListener() 
+		{
+			public void actionPerformed(ActionEvent e) 
+			{
 				ventana.cambio_panel(ventana.login, ventana.seleccionar);
 			}
 		});
--- src/main/java/Controlador/controladorRegistro.java
+++ src/main/java/Controlador/controladorRegistro.java
@@ -16,35 +16,44 @@ public class controladorRegistro {
 
 	private Ventana ventana;
 	private modelo modelo;
-	private Controlador Controlador;
 
-	public controladorRegistro(Ventana ventana, modelo modelo) {
+	public controladorRegistro(Ventana ventana, modelo modelo) 
+	{
 		this.ventana = ventana;
 		this.modelo = modelo;
 
-		ventana.registro.btnRegistro.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
+		ventana.registro.btnRegistro.addActionListener(new ActionListener() 
+		{
+			public void actionPerformed(ActionEvent e) 
+			{
 				if (funcionesLogin.comprobarCampos(ventana.registro.formattedTextFieldDNI.getText(),
 						ventana.registro.formattedTextFieldNombre.getText(),
 						ventana.registro.formattedTextFieldApellido.getText(),
-						String.valueOf(ventana.registro.passwordFieldClave.getPassword())) == true) {
-					JOptionPane.showMessageDialog(null, "Debes rellenar todos los campos...");
-				} else if (funcionesLogin.comprobarDni(GestorBD.consulta(funcionesLogin
-						.sentenciaCombrobarDni(ventana.registro.formattedTextFieldDNI.getText()))) == true) {
+						String.valueOf(ventana.registro.passwordFieldClave.getPassword())) == true) 
+				{
+						JOptionPane.showMessageDialog(null, "Debes rellenar todos los campos...");
+				}
+				else if (funcionesLogin.comprobarDni(GestorBD.consulta(funcionesLogin
+						.sentenciaCombrobarDni(ventana.registro.formattedTextFieldDNI.getText()))) == true) 
+				{
 					JOptionPane.showMessageDialog(null, "El usuario ya existe...");
-				} else if (GestorBD.insertarDatos(
+				} 
+				else if (GestorBD.insertarDatos(
 						funcionesLogin.sentenciaInsertarUsuario(ventana.registro.formattedTextFieldDNI.getText(),
 								ventana.registro.formattedTextFieldNombre.getText(),
 								ventana.registro.formattedTextFieldApellido.getText(),
-								String.valueOf(ventana.registro.passwordFieldClave.getPassword()))) == true) {
+								String.valueOf(ventana.registro.passwordFieldClave.getPassword()))) == true) 
+				{
 					JOptionPane.showMessageDialog(null, "Usuario creado con exito...");
 					ventana.cambio_panel(ventana.registro, ventana.seleccionar);
 				}
 			}
 		});
 
-		ventana.registro.btnCancelar.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
+		ventana.registro.btnCancelar.addActionListener(new ActionListener() 
+		{
+			public void actionPerformed(ActionEvent e) 
+			{
 				ventana.cambio_panel(ventana.registro, ventana.seleccionar);
 			}
 		});
