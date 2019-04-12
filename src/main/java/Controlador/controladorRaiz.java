b0626914dba4666fd0da57fe7209aac344e777ed
Vic <victorsantot@hotmail.com>
2019-04-12 21:10:15 +0200

    Update controladorRaiz.java

--- src/main/java/Controlador/controladorRaiz.java
+++ src/main/java/Controlador/controladorRaiz.java
@@ -27,14 +27,17 @@ public class controladorRaiz {
 		this.modelo = modelo;
 
 		// llenar el comboBox de ubicaciones
-		try {
+		try 
+		{
 			llenarUbicaciones(GestorBD.obtenerUbicaciones());
-		} catch (Exception e1) {
+		} 
+		catch (Exception e1) {
 			e1.printStackTrace();
 		}
 
 		// Calendario entrada
-		ventana.raiz.fechaIn.getDateEditor().addPropertyChangeListener(new PropertyChangeListener() {
+		ventana.raiz.fechaIn.getDateEditor().addPropertyChangeListener(new PropertyChangeListener()
+		{
 
 			@Override
 			public void propertyChange(PropertyChangeEvent evt) {
@@ -42,9 +45,12 @@ public class controladorRaiz {
 				// posterior al de entrada y que el dia de salida y entrada no sean el mismo
 				if (ventana.raiz.fechaIn.getDate() != null && ventana.raiz.fechaOut.getDate() != null
 						&& (ventana.raiz.fechaIn.getDate().before(ventana.raiz.fechaOut.getDate()))
-						&& !(ventana.raiz.fechaIn.getDate().equals(ventana.raiz.fechaOut.getDate()))) {
+						&& !(ventana.raiz.fechaIn.getDate().equals(ventana.raiz.fechaOut.getDate()))) 
+				{
 					ventana.raiz.btnBuscar.setEnabled(true);
-				} else {
+				}
+				else 
+				{
 					ventana.raiz.btnBuscar.setEnabled(false);
 				}
 
@@ -53,17 +59,22 @@ public class controladorRaiz {
 		});
 
 		// Calendario salida
-		ventana.raiz.fechaOut.getDateEditor().addPropertyChangeListener(new PropertyChangeListener() {
+		ventana.raiz.fechaOut.getDateEditor().addPropertyChangeListener(new PropertyChangeListener() 
+		{
 
 			@Override
-			public void propertyChange(PropertyChangeEvent evt) {
+			public void propertyChange(PropertyChangeEvent evt) 
+			{
 				// Comprobar que se hayan seleccionado dos fechas, que el día de salida sea
 				// posterior al de entrada y que el dia de salida y entrada no sean el mismo
 				if (ventana.raiz.fechaIn.getDate() != null && ventana.raiz.fechaOut.getDate() != null
 						&& (ventana.raiz.fechaIn.getDate().before(ventana.raiz.fechaOut.getDate()))
-						&& !(ventana.raiz.fechaIn.getDate().getDay() == ventana.raiz.fechaOut.getDate().getDay())) {
+						&& !(ventana.raiz.fechaIn.getDate().getDay() == ventana.raiz.fechaOut.getDate().getDay())) 
+				{
 					ventana.raiz.btnBuscar.setEnabled(true);
-				} else {
+				} 
+				else 
+				{
 					ventana.raiz.btnBuscar.setEnabled(false);
 				}
 
@@ -71,27 +82,35 @@ public class controladorRaiz {
 		});
 
 		// boton sumar Adulto
-		ventana.raiz.btnSumAdult.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
+		ventana.raiz.btnSumAdult.addActionListener(new ActionListener() 
+		{
+			public void actionPerformed(ActionEvent e) 
+			{
 				int maxKids = Integer.parseInt(ventana.raiz.txtKids.getText());
 				num_huespedes++;
 				ventana.raiz.txthuesped.setText(Integer.toString(num_huespedes));
-				if(num_huespedes > 11) {
+				if(num_huespedes > 11) 
+				{
 					ventana.raiz.btnSumAdult.setEnabled(false);
 				} 
-				else if(num_huespedes== 12 || num_huespedes==11) {
+				else if(num_huespedes== 12 || num_huespedes==11)
+				{
 					ventana.raiz.btnSumKids.setEnabled(false);
 					ventana.raiz.btnRestKids.setEnabled(false);
 					ventana.raiz.txtKids.setText("0");
-				}}
+				}
+			}
 			
 		});
 		
 
 		// boton restar Adulto
-		ventana.raiz.btnRestAdult.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				if (num_huespedes > 1 || num_huespedes<11) {
+		ventana.raiz.btnRestAdult.addActionListener(new ActionListener() 
+		{
+			public void actionPerformed(ActionEvent e)
+			{
+				if (num_huespedes > 1 || num_huespedes<11) 
+				{
 					num_huespedes--;
 					ventana.raiz.txthuesped.setText(Integer.toString(num_huespedes));
 					ventana.raiz.btnSumAdult.setEnabled(true);
@@ -102,27 +121,40 @@ public class controladorRaiz {
 		});
 		
 		// boton sumar Niño
-		ventana.raiz.btnSumKids.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
+		ventana.raiz.btnSumKids.addActionListener(new ActionListener() 
+		{
+			public void actionPerformed(ActionEvent e) 
+			{
 				num_kids++;
 				ventana.raiz.txtKids.setText(Integer.toString(num_kids));
 				ventana.raiz.btnSumKids.setEnabled(true);
-				if(num_kids > 5) {
+				if(num_kids > 5) 
+				{
 					ventana.raiz.btnSumKids.setEnabled(false);
 					
-				} else if(num_huespedes ==9 || num_huespedes ==10 && num_kids >=2) { //cuando haya 9 /10 adultos maximo niños 2
+				} 
+				else if(num_huespedes ==9 || num_huespedes ==10 && num_kids >=2) 
+				{ //cuando haya 9 /10 adultos maximo niños 2
 					ventana.raiz.btnSumKids.setEnabled(false);
 					
-				} else if(num_huespedes == 8 || num_huespedes == 7 && num_kids >=4) { // cuando haya 8 adultos maximo niños 4
+				} 
+				else if(num_huespedes == 8 || num_huespedes == 7 && num_kids >=4) 
+				{ // cuando haya 8 adultos maximo niños 4
 					ventana.raiz.btnSumKids.setEnabled(false);
 				
-				}else if(num_huespedes ==6 || num_huespedes ==5 || num_huespedes ==4 && num_kids >=6) { //cuando haya 6 adultos maximo niños 6
+				}
+				else if(num_huespedes ==6 || num_huespedes ==5 || num_huespedes ==4 && num_kids >=6) 
+				{ //cuando haya 6 adultos maximo niños 6
 					ventana.raiz.btnSumKids.setEnabled(false);
 				
-				}else if(num_huespedes ==3 || num_huespedes ==2 && num_kids >=4) {
+				}
+				else if(num_huespedes ==3 || num_huespedes ==2 && num_kids >=4) 
+				{
 					ventana.raiz.btnSumKids.setEnabled(false);//cuando haya 3 adultos maximo niños 4
 				
-				}else if(num_huespedes ==1 && num_kids >=2) {
+				}
+				else if(num_huespedes ==1 && num_kids >=2)
+				{
 					ventana.raiz.btnSumKids.setEnabled(false); //cuando haya 1 adulto maximo niños 2
 				}
 				
@@ -130,47 +162,68 @@ public class controladorRaiz {
 		});
 
 		// boton restar Niño
-		ventana.raiz.btnRestKids.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				if (num_kids > 1) {
+		ventana.raiz.btnRestKids.addActionListener(new ActionListener() 
+		{
+			public void actionPerformed(ActionEvent e) 
+			{
+				if (num_kids > 1) 
+				{
 					num_kids--;
 					ventana.raiz.txtKids.setText(Integer.toString(num_kids));
 					
-				}else if(num_huespedes ==9 || num_huespedes ==10 && num_kids <2) { // cuando haya 9 adultos y menos de 2 niños activar btnSumKids
+				}
+				else if(num_huespedes ==9 || num_huespedes ==10 && num_kids <2) 
+				{ // cuando haya 9 adultos y menos de 2 niños activar btnSumKids
 					ventana.raiz.btnSumKids.setEnabled(true);
 					
-				} else if(num_huespedes == 8 || num_huespedes == 7 && num_kids <4) {
+				} 
+				else if(num_huespedes == 8 || num_huespedes == 7 && num_kids <4) 
+				{
 					ventana.raiz.btnSumKids.setEnabled(true); //cuando haya 8 adultos y menos de 4 niños activar btnSumKids
 				
-				}else if(num_huespedes ==6 || num_huespedes == 5 || num_huespedes == 4 && num_kids <6) { //cuando haya 6 adultos y menos de 6 niños activar btnSumKids
+				}
+				else if(num_huespedes ==6 || num_huespedes == 5 || num_huespedes == 4 && num_kids <6) 
+				{ //cuando haya 6 adultos y menos de 6 niños activar btnSumKids
 					ventana.raiz.btnRestKids.setEnabled(true); 
 				
-				}else if(num_huespedes == 3 || num_huespedes ==2 && num_kids <4) { //cuando haya 3 adultos y menos de 4 niños activar btnSumKids
+				}
+				else if(num_huespedes == 3 || num_huespedes ==2 && num_kids <4) 
+				{ //cuando haya 3 adultos y menos de 4 niños activar btnSumKids
 					ventana.raiz.btnRestKids.setEnabled(true);
 				
-				}else if(num_huespedes == 1 && num_kids <2) { //cuando haya 1 adultos y menos de 2 niños activar btnSumKids
+				}
+				else if(num_huespedes == 1 && num_kids <2) 
+				{ //cuando haya 1 adultos y menos de 2 niños activar btnSumKids
 					ventana.raiz.btnRestKids.setEnabled(true);
 				}
 			}
 		});
 
 		// boton sumar Habitacion
-		ventana.raiz.btnSumHab.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
+		ventana.raiz.btnSumHab.addActionListener(new ActionListener() 
+		{
+			public void actionPerformed(ActionEvent e) 
+			{
 				num_Hab++;
 				ventana.raiz.txtHab.setText(Integer.toString(num_Hab));
-				if(num_Hab < 3) {
+				if(num_Hab < 3) 
+				{
 					ventana.raiz.btnSumHab.setEnabled(true);
-				}else{
+				}
+				else
+				{
 					ventana.raiz.btnSumHab.setEnabled(false);
 				}
 			}
 		});
 
 		// boton restar Habitacion
-		ventana.raiz.btnRestHab.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				if (num_Hab > 1 || num_Hab <3) {
+		ventana.raiz.btnRestHab.addActionListener(new ActionListener() 
+		{
+			public void actionPerformed(ActionEvent e) 
+			{
+				if (num_Hab > 1 || num_Hab <3) 
+				{
 					num_Hab--;
 					ventana.raiz.txtHab.setText(Integer.toString(num_Hab));
 					ventana.raiz.btnSumHab.setEnabled(true);
@@ -179,19 +232,24 @@ public class controladorRaiz {
 		});
 		
 		// boton pasar siguiente ventana
-		ventana.raiz.btnBuscar.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
+		ventana.raiz.btnBuscar.addActionListener(new ActionListener() 
+		{
+			public void actionPerformed(ActionEvent e) 
+			{
 				String ubicacionSeleccionada = ventana.raiz.comboBoxUbicacion.getSelectedItem().toString();
 
 				Date fechaIn = ventana.raiz.fechaIn.getDate();
 				Date fechaOut = ventana.raiz.fechaOut.getDate();
 
-				try {
+				try 
+				{
 
 					vistaHoteles.lm.llenarLista(ubicacionSeleccionada);
 //					ModeloLista.llenarLista(ubicacionSeleccionada);
 
-				} catch (Exception e1) {
+				} 
+				catch (Exception e1) 
+				{
 					System.out.println("el ArrayList de parada no ha sido rellenado");
 					e1.printStackTrace();
 				}
@@ -211,8 +269,10 @@ public class controladorRaiz {
 
 	}
 
-	public void llenarUbicaciones(ArrayList<String> ubicaciones) {
-		for (int i = 0; i < ubicaciones.size(); i++) {
+	public void llenarUbicaciones(ArrayList<String> ubicaciones) 
+	{
+		for (int i = 0; i < ubicaciones.size(); i++) 
+		{
 			ventana.raiz.comboBoxUbicacion.addItem(ubicaciones.get(i));
 		}
 	}
