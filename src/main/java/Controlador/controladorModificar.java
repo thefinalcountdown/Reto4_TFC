5dda8cefb8b604aa90b39eb9747c1a3d28e56f3e
Vic <victorsantot@hotmail.com>
2019-04-12 11:57:06 +0200

    Agregado metodo restablecer_datos en el controladorModificar (antes se quedaban los datos guardados) y al actualizar o borrar un usuario, vuelve a la pantalla de login

--- src/main/java/Controlador/controladorModificar.java
+++ src/main/java/Controlador/controladorModificar.java
@@ -15,49 +15,80 @@ public class controladorModificar {
 	private modelo modelo;
 	private Controlador Controlador;
 
-	public controladorModificar(Ventana ventana, modelo modelo) {
+	public controladorModificar(Ventana ventana, modelo modelo) 
+	{
 		this.ventana = ventana;
 		this.modelo = modelo;
 
-		ventana.modificar.btnActualizar.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
+		ventana.modificar.btnActualizar.addActionListener(new ActionListener() 
+		{
+			public void actionPerformed(ActionEvent arg0) 
+			{
 				if (funcionesLogin.comprobarDni(GestorBD.consulta(funcionesLogin.sentenciaCombrobarDni(
-						ventana.modificar.formattedTextFieldActualizarDNI.getText()))) == false) {
-					JOptionPane.showMessageDialog(null, "El usuario no existe...");
-				} else if (funcionesLogin.comprobarLogin(GestorBD.consulta(funcionesLogin.sentenciaLogin(
+						ventana.modificar.formattedTextFieldActualizarDNI.getText()))) == false) 
+				{
+					JOptionPane.showMessageDialog(null, "El usuario no existe.");
+				}
+				else if (funcionesLogin.comprobarLogin(GestorBD.consulta(funcionesLogin.sentenciaLogin(
 						ventana.modificar.formattedTextFieldActualizarDNI.getText(),
-						String.valueOf(ventana.modificar.passwordFieldActualizarClave.getPassword())))) == false) {
-					JOptionPane.showMessageDialog(null, "Clave incorrecta...");
-				} else if (GestorBD.insertarDatos(funcionesLogin.sentenciaActualizarUsuario(
+						String.valueOf(ventana.modificar.passwordFieldActualizarClave.getPassword())))) == false) 
+				{
+					JOptionPane.showMessageDialog(null, "Clave incorrecta.");
+				} 
+				else if (GestorBD.insertarDatos(funcionesLogin.sentenciaActualizarUsuario(
 						ventana.modificar.formattedTextFieldActualizarDNI.getText(),
 						String.valueOf(ventana.modificar.passwordFieldActualizarClave.getPassword()),
-						String.valueOf(ventana.modificar.passwordFieldNuevaClave.getPassword()))) == true) {
-					JOptionPane.showMessageDialog(null, "Clave actualizada con exito...");
+						String.valueOf(ventana.modificar.passwordFieldNuevaClave.getPassword()))) == true) 
+				{
+					JOptionPane.showMessageDialog(null, "Clave actualizada con exito.");
+					ventana.cambio_panel(ventana.modificar, ventana.login);
+					restablecer_datos();
 				}
 			}
 		});
 
-		ventana.modificar.btnBorrarUsuario.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
+		ventana.modificar.btnBorrarUsuario.addActionListener(new ActionListener() 
+		{
+			public void actionPerformed(ActionEvent arg0) 
+			{
 				if (funcionesLogin.comprobarDni(GestorBD.consulta(funcionesLogin
-						.sentenciaCombrobarDni(ventana.modificar.formattedTextFieldBorrarDni.getText()))) == false) {
+						.sentenciaCombrobarDni(ventana.modificar.formattedTextFieldBorrarDni.getText()))) == false) 
+				{
 					JOptionPane.showMessageDialog(null, "El usuario no existe...");
-				} else if (funcionesLogin.comprobarLogin(GestorBD
+				} 
+				else if (funcionesLogin.comprobarLogin(GestorBD
 						.consulta(funcionesLogin.sentenciaLogin(ventana.modificar.formattedTextFieldBorrarDni.getText(),
-								String.valueOf(ventana.modificar.passwordFieldBorrarDni.getPassword())))) == false) {
+								String.valueOf(ventana.modificar.passwordFieldBorrarDni.getPassword())))) == false) 
+				{
 					JOptionPane.showMessageDialog(null, "Clave incorrecta...");
-				} else if (GestorBD.insertarDatos(
+				} 
+				else if (GestorBD.insertarDatos(
 						funcionesLogin.sentenciaBorrarUsuario(ventana.modificar.formattedTextFieldBorrarDni.getText(),
-								String.valueOf(ventana.modificar.passwordFieldBorrarDni.getPassword()))) == true) {
-					JOptionPane.showMessageDialog(null, "Usuario borrado correctamente");
+								String.valueOf(ventana.modificar.passwordFieldBorrarDni.getPassword()))) == true) 
+				{
+					JOptionPane.showMessageDialog(null, "Usuario borrado correctamente.");
+					ventana.cambio_panel(ventana.modificar, ventana.login);
+					restablecer_datos();
 				}
 			}
 		});
 
 		ventana.modificar.btnVolver.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
+			public void actionPerformed(ActionEvent arg0) 
+			{
 				ventana.cambio_panel(ventana.modificar, ventana.login);
+				restablecer_datos();
 			}
 		});
+	}
+	
+	public void restablecer_datos()
+	{
+		ventana.modificar.formattedTextFieldActualizarDNI.setText("");
+		ventana.modificar.formattedTextFieldBorrarDni.setText("");
+		ventana.modificar.passwordFieldActualizarClave.setText("");
+		ventana.modificar.passwordFieldBorrarDni.setText("");
+		ventana.modificar.passwordFieldNuevaClave.setText("");
+		
 	}
 }
