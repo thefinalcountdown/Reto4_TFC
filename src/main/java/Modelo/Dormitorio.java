7bb4aa95bb4e83b5d963167c1799c41d1169eb8e
Vic <victorsantot@hotmail.com>
2019-04-29 21:10:01 +0200

    Update Dormitorio.java

--- src/main/java/Modelo/Dormitorio.java
+++ src/main/java/Modelo/Dormitorio.java
@@ -2,60 +2,64 @@ package Modelo;
 
 public class Dormitorio extends Habitaciones {
 	
-	String cod_Dormi;
+	String cod_dormitorio;
 	int camaIndividual;
 	int camaInfantil;
 	int camaMatrimonio;
 	
-	public Dormitorio(float metrosCuadrados, String RelacionMobiliario, int CamaIndividual, int CamaInfantil, int CamaMatrimonio) {
+	
+	public Dormitorio(String cod_dormitorio, float metrosCuadrados, int camaIndividual, int camaInfantil, int camaMatrimonio) {
 		super(metrosCuadrados);
-		this.cod_Dormi = RelacionMobiliario;
-		this.camaIndividual = CamaIndividual;
-		this.camaInfantil = CamaInfantil;
-		this.camaMatrimonio = CamaMatrimonio;
+		this.cod_dormitorio = cod_dormitorio;
+		this.camaIndividual = camaIndividual;
+		this.camaInfantil = camaInfantil;
+		this.camaMatrimonio = camaMatrimonio;
 	}
 
-	public float getMetrosCuadrados() {
-		return metrosCuadrados;
+	public String getCod_dormitorio() {
+		return cod_dormitorio;
 	}
 
-	public void setMetrosCuadrados(float metrosCuadrados) {
-		this.metrosCuadrados = metrosCuadrados;
+
+	public void setCod_dormitorio(String cod_dormitorio) {
+		this.cod_dormitorio = cod_dormitorio;
 	}
 
-	public String getRelacionMobiliario() {
-		return cod_Dormi;
-	}
-
-	public void setRelacionMobiliario(String relacionMobiliario) {
-		this.cod_Dormi = relacionMobiliario;
-	}
 
 	public int getCamaIndividual() {
 		return camaIndividual;
 	}
 
+
 	public void setCamaIndividual(int camaIndividual) {
 		this.camaIndividual = camaIndividual;
 	}
+
+
 	public int getCamaInfantil() {
 		return camaInfantil;
 	}
 
+
 	public void setCamaInfantil(int camaInfantil) {
 		this.camaInfantil = camaInfantil;
 	}
+
+
 	public int getCamaMatrimonio() {
 		return camaMatrimonio;
 	}
 
+
 	public void setCamaMatrimonio(int camaMatrimonio) {
 		this.camaMatrimonio = camaMatrimonio;
 	}
-	
+
+
 	@Override
 	public String toString() {
-		return super.toString()+" "+this.cod_Dormi+" "+this.camaIndividual+" "+this.camaInfantil+" "+this.camaMatrimonio;
-		//return "Dormitorio [metros cuadrados=" + metrosCuadrados+ ", relacione mobiliario=" + relacionMobiliario + ", camas disponibles=" + camasDisponibles+"]";
+		return "Dormitorio [cod_dormitorio=" + cod_dormitorio + ", camaIndividual="
+				+ camaIndividual + ", camaInfantil=" + camaInfantil + ", camaMatrimonio=" + camaMatrimonio + "]";
 	}
+
 }
