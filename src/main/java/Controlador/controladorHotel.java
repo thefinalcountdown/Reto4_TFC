3c8f097ce794c73c0b9db1df09244f6f5e7eb8b2
Pivorruchi <ivorrairis@gmail.com>
2019-04-29 13:58:00 +0200

    Mostrar_camas
    Muestra todas las habitaciones del hotel seleccionado con sus camas; solo muestra las habitaciones que existen (si el hotel no tiene 10 habitaciones, sino 7 muestra solo las 7)

--- src/main/java/Controlador/controladorHotel.java
+++ src/main/java/Controlador/controladorHotel.java
@@ -53,18 +53,19 @@ public class controladorHotel {
 		ventana.hotel.btnHabitaciones.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 
-				//vaciamos la lista cada vez que se le da al boton para que no conserve lo de otros hoteles seleccionados
+				// vaciamos la lista cada vez que se le da al boton para que no conserve lo de
+				// otros hoteles seleccionados
 				try {
 					modelo.modeloListaHabitacion.vaciarLista();
 				} catch (Exception e1) {
 					// TODO Auto-generated catch block
 					e1.printStackTrace();
 				}
-				//Este sysout era para ver que cogia el codHotel, podemos borrarlo
+				// Este sysout era para ver que cogia el codHotel, podemos borrarlo
 				System.out.println(modelo.modeloListaHotel.hoteles.get(ventana.hotel.listaHoteles.getSelectedIndex())
 						.getCod_alojamiento());
-				Hotel1= new Hotel_habitacion();
-				//el Hotel1 que es el objeto Hotel_Habitacion se rellena 
+				Hotel1 = new Hotel_habitacion();
+				// el Hotel1 que es el objeto Hotel_Habitacion se rellena
 				try {
 					obtenerHabitaciones(modelo.modeloListaHotel.hoteles
 							.get(ventana.hotel.listaHoteles.getSelectedIndex()).getCod_alojamiento());
@@ -72,17 +73,22 @@ public class controladorHotel {
 					// TODO Auto-generated catch block
 					e1.printStackTrace();
 				}
-				//usamos el metodo para rellenar el modelo lista con los dormitorios, ahora mismo solo coge la Habitacion1 con el getter
+				// usamos el metodo para rellenar el modelo lista con los dormitorios, ahora
+				// mismo solo coge la Habitacion1 con el getter
 				try {
-					modelo.modeloListaHabitacion.llenarDormitorios(Hotel1.getHabitacion1());
+					modelo.modeloListaHabitacion.llenarDormitorios(Hotel1.getHabitacion1(), Hotel1.getHabitacion2(),
+							Hotel1.getHabitacion3(), Hotel1.getHabitacion4(), Hotel1.getHabitacion5(),
+							Hotel1.getHabitacion6(), Hotel1.getHabitacion7(), Hotel1.getHabitacion8(),
+							Hotel1.getHabitacion9(), Hotel1.getHabitacion10());
+
 					ventana.hotel.listaHabitaciones.setModel(modelo.modeloListaHabitacion);
-					
+
 				} catch (Exception e1) {
 					System.out.println("el ArrayList de dormitorios no ha sido rellenado");
 					e1.printStackTrace();
 				}
-				//Sysout para ver que se rellena el objeto correctamente
-				System.out.println(Hotel1.toString()+"heyyyy");
+				// Sysout para ver que se rellena el objeto correctamente
+				System.out.println(Hotel1.toString() + "heyyyy");
 
 			}
 		});
@@ -91,6 +97,7 @@ public class controladorHotel {
 			public void actionPerformed(ActionEvent e) {
 				ventana.cambio_panel(ventana.hotel, ventana.raiz);
 				try {
+					modelo.modeloListaHabitacion.vaciarLista();
 					modelo.modeloListaHotel.vaciarLista();
 				} catch (Exception e1) {
 					// TODO Auto-generated catch block
@@ -102,21 +109,21 @@ public class controladorHotel {
 		});
 
 	}
-	
-	//metodo para rellenar el objeto 
 
-	public static Hotel_habitacion obtenerHabitaciones(int i)throws Exception {
+	// metodo para rellenar el objeto
+
+	public static Hotel_habitacion obtenerHabitaciones(int i) throws Exception {
 
 		String sentencia = "select * from Hotel_habitacion where Cod_Hotel='%s'";
 		sentencia = String.format(sentencia, i);
 		ResultSet result = GestorBD.consulta(sentencia);
 		while (result.next()) {
-			Hotel1=new Hotel_habitacion(result.getInt("Cod_Hotel"), result.getString("Habitacion1"), result.getString("Habitacion2"),
-					result.getString("Habitacion3"), result.getString("Habitacion4"), result.getString("Habitacion5"), 
-					result.getString("Habitacion6"), result.getString("Habitacion7"), result.getString("Habitacion8"), 
-					result.getString("Habitacion9"), result.getString("Habitacion10"));
+			Hotel1 = new Hotel_habitacion(result.getInt("Cod_Hotel"), result.getString("Habitacion1"),
+					result.getString("Habitacion2"), result.getString("Habitacion3"), result.getString("Habitacion4"),
+					result.getString("Habitacion5"), result.getString("Habitacion6"), result.getString("Habitacion7"),
+					result.getString("Habitacion8"), result.getString("Habitacion9"), result.getString("Habitacion10"));
 		}
-return Hotel1;
+		return Hotel1;
 	}
 
 }
--- src/main/java/Modelo/ModeloListaHabitaciones.java
+++ src/main/java/Modelo/ModeloListaHabitaciones.java
@@ -9,51 +9,102 @@ import javax.swing.event.ListDataListener;
 import Vista.Ventana;
 import metodos.GestorBD;
 
-public class ModeloListaHabitaciones implements ListModel{
+public class ModeloListaHabitaciones implements ListModel {
 	Ventana ventana;
 	public ArrayList<String> arrayString = new ArrayList<String>();
 	public static ArrayList<Dormitorio> dormitorios = new ArrayList<Dormitorio>();
-	
+	static Dormitorio dormi;
 
-	public void llenarDormitorios(String ubicacion) throws Exception {
-		dormitorios = obtenerDormitorios(ubicacion);
+	public void llenarDormitorios(String cod_hab1, String cod_hab2, String cod_hab3, String cod_hab4, String cod_hab5,
+			String cod_hab6, String cod_hab7, String cod_hab8, String cod_hab9, String cod_hab10) throws Exception {
+		dormitorios = obtenerDormitorios(cod_hab1, cod_hab2, cod_hab3, cod_hab4, cod_hab5, cod_hab6, cod_hab7, cod_hab8,
+				cod_hab9, cod_hab10);
 
-		for (int index = 0; index < dormitorios.size(); index++) 
-		{
-			makeObj(dormitorios.get(index).getCamaIndividual(), dormitorios.get(index).getCamaInfantil(), dormitorios.get(index).getCamaMatrimonio());
-			arrayString.add("Camas individuales: "+dormitorios.get(index).getCamaIndividual() + "  Camas de matrimonio:" + dormitorios.get(index).getCamaMatrimonio()
-					+"  Camas infantiles: "+dormitorios.get(index).getCamaInfantil());
+		for (int index = 0; index < dormitorios.size(); index++) {
+			makeObj(dormitorios.get(index).getCamaIndividual(), dormitorios.get(index).getCamaInfantil(),
+					dormitorios.get(index).getCamaMatrimonio());
+			arrayString.add("Camas individuales: " + dormitorios.get(index).getCamaIndividual()
+					+ "  Camas de matrimonio:" + dormitorios.get(index).getCamaMatrimonio() + "  Camas infantiles: "
+					+ dormitorios.get(index).getCamaInfantil());
 		}
 	}
-	
-	public void vaciarLista(){
-		
-		for(int index = arrayString.size()-1; index >= 0; index--)
-		{
+
+	public void vaciarLista() {
+
+		for (int index = arrayString.size() - 1; index >= 0; index--) {
 			dormitorios.remove(index);
 			arrayString.remove(index);
 		}
-		
+
 	}
 
-	
-	public static ArrayList<Dormitorio> obtenerDormitorios(String cod_hab) throws Exception {
+	public static ArrayList<Dormitorio> obtenerDormitorios(String cod_hab1, String cod_hab2, String cod_hab3,
+			String cod_hab4, String cod_hab5, String cod_hab6, String cod_hab7, String cod_hab8, String cod_hab9,
+			String cod_hab10) throws Exception {
 
 		String sentencia = "select * from Hab_Dormitorio where Cod_Habitacion='%s'";
-		sentencia = String.format(sentencia, cod_hab);
-		ResultSet result = GestorBD.consulta(sentencia);
-		while (result.next()) {
-			dormitorios.add(new Dormitorio(result.getFloat("Superficie"), result.getString("Cod_Habitacion"), result.getInt("Cama_individual"),
-					result.getInt("Cama_infantil"), result.getInt("Cama_matrimonio")));
+		if (cod_hab1!=null) {
+		String sentencia1 = String.format(sentencia, cod_hab1);
+		dormitorios.add(codigosHab(sentencia1));
 		}
+		if (cod_hab2!=null) {
+		String sentencia2 = String.format(sentencia, cod_hab2);
+		dormitorios.add(codigosHab(sentencia2));
+		}
+		if (cod_hab3!=null) {
+		String sentencia3 = String.format(sentencia, cod_hab3);
+		dormitorios.add(codigosHab(sentencia3));
+		}
+		if (cod_hab4!=null) {
+		String sentencia4 = String.format(sentencia, cod_hab4);
+		dormitorios.add(codigosHab(sentencia4));
+		}
+		if (cod_hab5!=null) {
+		String sentencia5 = String.format(sentencia, cod_hab5);
+		dormitorios.add(codigosHab(sentencia5));
+		}
+		if (cod_hab6!=null) {
+		String sentencia6 = String.format(sentencia, cod_hab6);
+		dormitorios.add(codigosHab(sentencia6));
+		}
+		if (cod_hab7!=null) {
+		String sentencia7 = String.format(sentencia, cod_hab7);
+		dormitorios.add(codigosHab(sentencia7));
+		}
+		if (cod_hab8!=null) {
+		String sentencia8 = String.format(sentencia, cod_hab8);
+		dormitorios.add(codigosHab(sentencia8));
+		}
+		if (cod_hab9!=null) {
+		String sentencia9 = String.format(sentencia, cod_hab9);
+		dormitorios.add(codigosHab(sentencia9));
+		}
+		if (cod_hab10!=null) {
+		String sentencia10 = String.format(sentencia, cod_hab10);
+		dormitorios.add(codigosHab(sentencia10));
+		
+		System.out.println(sentencia10);
+		}
+
 		return dormitorios;
 
+	}
+
+	public static Dormitorio codigosHab(String sentencia) throws Exception {
+
+		ResultSet result = GestorBD.consulta(sentencia);
+		while (result.next()) {
+			dormi = new Dormitorio(result.getFloat("Superficie"), result.getString("Cod_Habitacion"),
+					result.getInt("Cama_individual"), result.getInt("Cama_infantil"), result.getInt("Cama_matrimonio"));
+		}
+
+		return dormi;
 	}
 
 	public static Object makeObj(final int item, int item2, int item3) {
 		return new Object() {
 			public String toString() {
-				return ""+item+item2+item3;
+				return "" + item + item2 + item3;
 			}
 		};
 	}
@@ -81,5 +132,5 @@ public class ModeloListaHabitaciones implements ListModel{
 		// TODO Auto-generated method stub
 
 	}
-	
+
 }
