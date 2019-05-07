e791e4d6b63d06fb79b7f96b66f3ff604fb4a2cb
Vic <victorsantot@hotmail.com>
2019-05-07 18:02:23 +0200

    Guardado en Mayusculas en BBDD

--- src/main/java/Controlador/controladorLogin.java
+++ src/main/java/Controlador/controladorLogin.java
@@ -17,46 +17,53 @@ public class controladorLogin {
 		this.ventana = ventana;
 		this.modelo = modelo;
 
-		ventana.login.btnLogin.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
+		ventana.login.btnLogin.addActionListener(new ActionListener() 
+		{
+			public void actionPerformed(ActionEvent e) 
+			{
 				// comprueba si el usuario existe en la base de datos
 				if (modelo.modeloLogin.comprobarDni(GestorBD.consulta(modelo.modeloLogin
-						.sentenciaCombrobarDni(ventana.login.formattedTextFieldLoginDNI.getText()))) == false)
+						.sentenciaCombrobarDni(ventana.login.formattedTextFieldLoginDNI.getText().toUpperCase()))) == false)
 				{
 					JOptionPane.showMessageDialog(null, "El usuario no existe.");
 					// comprueba que el dni sea valido a la hora de registrar usuario
 				} 
-				else if (modelo.modeloRegistro
-						.validarNIF(ventana.login.formattedTextFieldLoginDNI.getText()) == false) 
+				else if (modelo.modeloRegistro.validarNIF(ventana.login.formattedTextFieldLoginDNI.getText().toUpperCase()) == false) 
 				{
 					JOptionPane.showMessageDialog(null, "El DNI introducido no es v\u00e1lido...");
-					// intenta loguear con el usuario, si dni y contrase�a son correctos manda el
+					// intenta loguear con el usuario, si dni y contrasena son correctos manda el
 					// usuario a la reserva y cambia de panel
 				} 
-				else if (modelo.modeloLogin.comprobarLogin(GestorBD
-						.consulta(modelo.modeloLogin.sentenciaLogin(ventana.login.formattedTextFieldLoginDNI.getText(),
-								String.valueOf(ventana.login.passwordFieldLoginClave.getPassword())))) == true) {
+				else if (modelo.modeloLogin.comprobarLogin(GestorBD.consulta(modelo.modeloLogin.sentenciaLogin(ventana.login.formattedTextFieldLoginDNI.getText().toUpperCase(),
+								String.valueOf(ventana.login.passwordFieldLoginClave.getPassword())))) == true) 
+				{
 					JOptionPane.showMessageDialog(null, "Usuario logueado con exito.");
-					String nombre_apellidos = GestorBD
-							.conseguir_nombre_apellidos(ventana.login.formattedTextFieldLoginDNI.getText());
+					String nombre_apellidos = GestorBD.conseguir_nombre_apellidos(ventana.login.formattedTextFieldLoginDNI.getText().toUpperCase());
+					
 					ventana.reserva.textField_usuario.setText(nombre_apellidos);
 					ventana.reserva.textField_DNI.setText(ventana.login.formattedTextFieldLoginDNI.getText());
 					ventana.cambio_panel(ventana.login, ventana.raiz);
 					JOptionPane.showMessageDialog(ventana.raiz, "Bienvenid@ " + nombre_apellidos);
-				} else {
+				} 
+				else 
+				{
 					JOptionPane.showMessageDialog(null, "Clave incorrecta.");
 				}
 			}
 		});
 
-		ventana.login.btnRegistro.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
+		ventana.login.btnRegistro.addActionListener(new ActionListener() 
+		{
+			public void actionPerformed(ActionEvent arg0)
+			{
 				ventana.cambio_panel(ventana.login, ventana.registro);
 			}
 		});
 
-		ventana.login.btnModificar.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
+		ventana.login.btnModificar.addActionListener(new ActionListener() 
+		{
+			public void actionPerformed(ActionEvent arg0)
+			{
 				ventana.cambio_panel(ventana.login, ventana.modificar);
 
 			}
--- src/main/java/Controlador/controladorRegistro.java
+++ src/main/java/Controlador/controladorRegistro.java
@@ -25,26 +25,33 @@ public class controladorRegistro {
 				if (modelo.modeloRegistro.comprobarCampos(ventana.registro.formattedTextFieldDNI.getText(),
 						ventana.registro.formattedTextFieldNombre.getText(),
 						ventana.registro.formattedTextFieldApellido.getText(),
-						String.valueOf(ventana.registro.passwordFieldClave.getPassword())) == true) {
+						String.valueOf(ventana.registro.passwordFieldClave.getPassword())) == true) 
+				{
 					JOptionPane.showMessageDialog(null, "Debes rellenar todos los campos.");
 				//comprueba que el dni sea valido a la hora de registrar usuario
-				} else if (modelo.modeloRegistro
-						.validarNIF(ventana.registro.formattedTextFieldDNI.getText()) == false) {
+				} 
+				else if (modelo.modeloRegistro
+						.validarNIF(ventana.registro.formattedTextFieldDNI.getText()) == false) 
+				{
 					JOptionPane.showMessageDialog(null, "El DNI introducido no es v\u00e1lido...");
 				//comprueba si el usuario ya existe en la base de datos
-				} else if (modelo.modeloLogin.comprobarDni(GestorBD.consulta(modelo.modeloLogin
-						.sentenciaCombrobarDni(ventana.registro.formattedTextFieldDNI.getText()))) == true) {
+				}
+				else if (modelo.modeloLogin.comprobarDni(GestorBD.consulta(modelo.modeloLogin
+						.sentenciaCombrobarDni(ventana.registro.formattedTextFieldDNI.getText()))) == true) 
+				{
 					JOptionPane.showMessageDialog(null, "El usuario ya existe.");
 				//inserta el usuario en la base de datos, manda los datos a la reserva y cambia de panel
-				} else if (GestorBD.insertarDatos(
-						modelo.modeloRegistro.sentenciaInsertarUsuario(ventana.registro.formattedTextFieldDNI.getText(),
-								ventana.registro.formattedTextFieldNombre.getText(),
-								ventana.registro.formattedTextFieldApellido.getText(),
-								String.valueOf(ventana.registro.passwordFieldClave.getPassword()))) == true) {
+				}
+				else if (GestorBD.insertarDatos(
+						modelo.modeloRegistro.sentenciaInsertarUsuario(ventana.registro.formattedTextFieldDNI.getText().toUpperCase(),
+								ventana.registro.formattedTextFieldNombre.getText().toUpperCase(),
+								ventana.registro.formattedTextFieldApellido.getText().toUpperCase(),
+								String.valueOf(ventana.registro.passwordFieldClave.getPassword()))) == true) 
+				{
 					JOptionPane.showMessageDialog(null, "Usuario creado con exito.");
-					String nombre_apellidos = ventana.registro.formattedTextFieldNombre.getText() + " "
-							+ ventana.registro.formattedTextFieldApellido.getText();
-					ventana.reserva.textField_DNI.setText(ventana.registro.formattedTextFieldDNI.getText());
+					String nombre_apellidos = ventana.registro.formattedTextFieldNombre.getText().toUpperCase() + " "
+							+ ventana.registro.formattedTextFieldApellido.getText().toUpperCase();
+					ventana.reserva.textField_DNI.setText(ventana.registro.formattedTextFieldDNI.getText().toUpperCase());
 					ventana.reserva.textField_usuario.setText(nombre_apellidos);
 					ventana.cambio_panel(ventana.registro, ventana.raiz);
 					JOptionPane.showMessageDialog(ventana.raiz, "Bienvenid@ " + nombre_apellidos);
@@ -52,7 +59,8 @@ public class controladorRegistro {
 			}
 		});
 
-		ventana.registro.btnCancelar.addActionListener(new ActionListener() {
+		ventana.registro.btnCancelar.addActionListener(new ActionListener() 
+		{
 			public void actionPerformed(ActionEvent e) {
 				ventana.cambio_panel(ventana.registro, ventana.login);
 
